6fa49bd5b261f2e4b4a1f04f472e90908b02aa07
chrisminnick <chris@watzthis.com>
2017-02-12 20:56:32 -0800

    returning actual weather

--- bots/actions/index.js
+++ bots/actions/index.js
@@ -1,6 +1,9 @@
+//todo: make it handle error cases better
+
 const firstEntityValue = require('../utils/firstEntityValue.js');
 const sessions = require('../utils/sessions.js');
 const app = require('../witbot');
+var http = require('http');
 
 const actions = {
     send(request, response) {
@@ -26,7 +29,31 @@ const actions = {
 
         var location = firstEntityValue(entities, 'location');
         if (location) {
-            context.forecast = 'sunny in ' + location; // we should call a weather API here
+            const WEATHER_KEY = '16eba5a259d5a110';
+
+            var options = {
+                host: 'api.wunderground.com',
+                path: '/api/'+WEATHER_KEY+'/geolookup/conditions/q/'+encodeURI(location)+'.json'
+
+            };
+            var callback = function(response){
+                var str = '';
+                response.on('data', function(chunk) {
+                    str += chunk;
+                });
+                response.on('end', function() {
+                    console.log(str);
+                    str = JSON.parse(str);
+                    context.forecast = str.current_observation.temp_f + " degrees and " + str.current_observation.weather + " in " + location;
+                    /*spark.createMessage(message.roomId, "<@personEmail:" + trigger.data.personEmail + "> "+str.current_observation.temp_f+ " degrees (F). " + str.current_observation.weather, {"markdown": true}, function (err, message) {
+                        if (err) {
+                            console.log("WARNING: could not post message to room: " + command.message.roomId);
+                            return;
+                        }
+                    });*/
+                });
+            };
+            var request = http.get(options,callback).end();
             delete context.missingLocation;
         } else {
             context.missingLocation = true;
